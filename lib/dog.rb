224324eb69b379951c0d10dccae1dccfd9ff0481
earthctzn <earthctzn1@gmail.com>
2019-07-04 11:44:52 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -32,17 +32,17 @@ class Dog
           self.update
         else
           sql = <<-SQL
-            INSERT INTO #{Dog.table_name} (name, breed) 
+            INSERT INTO #{self.class.table_name} (name, breed) 
             VALUES (?, ?)
           SQL
           DB[:conn].execute(sql, self.name, self.breed)
-          @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{Dog.table_name}")[0][0]
+          @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{self.class.table_name}")[0][0]
           return self
         end
     end
 
     def self.create(name: name, breed: breed)
-        dog = Dog.new(name: name, breed: breed)
+        dog = self.new(name: name, breed: breed)
         dog.save
         dog
     end
@@ -51,25 +51,25 @@ class Dog
         id = dog_arry[0]
         name = dog_arry[1]
         breed = dog_arry[2]
-        Dog.new(id: id, name: name, breed: breed)
+        self.new(id: id, name: name, breed: breed)
     end
 
 
     def self.find_by_id(id)
         sql = "SELECT * FROM #{self.table_name} WHERE id = ?"
         dog = DB[:conn].execute(sql, id)[0]
-        Dog.new_from_db(dog)
+        self.new_from_db(dog)
     end
 
     def self.find_by_name(name)
         sql = "SELECT * FROM #{self.table_name} WHERE name = ?"
         dog = DB[:conn].execute(sql, name)[0]
-        Dog.new_from_db(dog)
+        self.new_from_db(dog)
     end
 
 
     def self.find_or_create_by(name:, breed:)
-        dog = DB[:conn].execute("SELECT * FROM #{Dog.table_name} WHERE name = ? AND breed = ?", name, breed)
+        dog = DB[:conn].execute("SELECT * FROM #{self.table_name} WHERE name = ? AND breed = ?", name, breed)
         if !dog.empty?
           dog_data = dog[0]
           dog = Dog.new_from_db(dog_data)
@@ -80,7 +80,7 @@ class Dog
     end 
     
     def update
-        sql = "UPDATE #{Dog.table_name} SET name = ?, breed = ?, id = ?"
+        sql = "UPDATE #{self.class.table_name} SET name = ?, breed = ?, id = ?"
         DB[:conn].execute(sql, self.name, self.breed, self.id)
     end
 end
